75ca6878a99948a37d0c9785ae3d6cc58ca57900
Brian Nickila <brian.nickila@gmail.com>
2019-04-22 10:32:40 -0500

    Changed some formatting and styles.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function Footer() {
     return (
-        <div>
-
+        <div className="container-fluid footer">
+            <div className="row">
+                <div className="col-md-12 fab-div">
+                    <a href="https://github.com/nickila"><i className="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/brian-nickila"><i className="fab fa-linkedin"></i></a>
+                </div>
+            </div >
         </div>
     )
 }
--- src/components/Name.js
+++ src/components/Name.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Name() {
     return (
@@ -6,13 +7,21 @@ function Name() {
             <div className="row main-row">
                 <div className="col-md-8">
                     <h1 className="name">Brian Nickila</h1>
-                    <p className="about"><span className="bold">Full-Stack Web Developer</span> creating apps and websites in a creative and user-friendly style in St. Paul, Minnesota. I have a bachelor of fine arts degree from the University of Wisconsin-Stevens Point and a certificate in full-stack web development from the University of Minnesota. When I'm not writing code, you might find me animating, playing guitar, gardening or camping. Please check out my <span className="bold link">portfolio</span> and feel free to <span className="bold link">contact</span> me for any reason. I'd love to hear from you.</p>
+                    <p className="about"><span className="bold">Full-Stack Web Developer</span> creating apps and websites in a creative and user-friendly style in St. Paul, Minnesota. I have a bachelor of fine arts degree from the University of Wisconsin-Stevens Point and a certificate in full-stack web development from the University of Minnesota. When I'm not writing code, you might find me working on <Link className="bold link" to="/animation">animation</Link>, playing guitar, gardening or camping. Please check out my <Link className="bold link" to="/portfolio">portfolio</Link> and feel free to <Link className="bold link" to="/contact">contact</Link> me for any reason. I'd love to hear from you.</p>
+                    <p className="tech"><span className="bold">Technologies:</span> React, JavaScript, HTML, CSS, jQuery, Bootstrap, MongoDB, MySQL, Express, Node.js, Sequelize, Mongoose, Photoshop, Illustrator & Flash</p>
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-4 img-div">
                     <img className="profile" src="images/profile1sm.png" />
                 </div>
             </div>
-        </div>
+            {/* <div className="row bottom-row">
+                <div className="col-md-10">
+                    <h3 className="techHeader">Technologies</h3>
+                    <hr />
+                    <p className="tech">React &nbsp; JavaScript &nbsp; HTML &nbsp; CSS &nbsp; jQuery &nbsp; Bootstrap &nbsp; MongoDB &nbsp; MySQL &nbsp; Express &nbsp; Node.js &nbsp; Sequelize &nbsp; Mongoose &nbsp; Photoshop &nbsp; Illustrator &nbsp; Flash</p>
+                </div>
+            </div> */}
+        </div >
     )
 }
 
