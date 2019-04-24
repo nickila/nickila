78392345b8243d36ec278bb58bc69f77380ce1b5
Brian Nickila <brian.nickila@gmail.com>
2019-04-24 11:12:49 -0500

    adjusted image to change on hover

--- src/components/Name.js
+++ src/components/Name.js
@@ -7,11 +7,17 @@ function Name() {
             <div className="row main-row">
                 <div className="col-md-8">
                     <h1 className="name">Brian Nickila</h1>
-                    <p className="about"><span className="bold">Full-Stack Web Developer</span> creating apps and websites in a creative and user-friendly style in St. Paul, Minnesota. I have a bachelor of fine arts degree from the University of Wisconsin-Stevens Point and a certificate in full-stack web development from the University of Minnesota. When I'm not writing code, you might find me working on <Link className="bold link" to="/animation">animation</Link>, playing guitar, gardening or camping. Please check out my <Link className="bold link" to="/portfolio">portfolio</Link> and feel free to <Link className="bold link" to="/contact">contact</Link> me for any reason. I'd love to hear from you.</p>
+                    <p className="about"><span className="bold">Front-End Web Developer</span> creating apps and websites in a creative and user-friendly style in St. Paul, Minnesota. I have a bachelor of fine arts degree from the University of Wisconsin-Stevens Point and a certificate in full-stack web development from the University of Minnesota. When I'm not writing code, you might find me working on <Link className="bold link" to="/animation">animation</Link>, playing guitar, gardening or camping. Please check out my <Link className="bold link" to="/portfolio">portfolio</Link> and feel free to <Link className="bold link" to="/contact">contact</Link> me for any reason. I'd love to hear from you.</p>
                     <p className="tech"><span className="bold">Technologies:</span> React, JavaScript, HTML, CSS, jQuery, Bootstrap, MongoDB, MySQL, Express, Node.js, Sequelize, Mongoose, Photoshop, Illustrator & Flash</p>
                 </div>
                 <div className="col-md-4 img-div">
-                    <img className="profile" src="images/profile1sm.png" />
+                    <img className="profile" src="images/profile1.png" />
+                    <div className="overlay">
+                        <img className="color-profile" src="images/profile2.png" />
+                    </div>
+                    {/* <img className="profile" src="images/profile1.png"
+                        onMouseOver={e => (e.currentTarget.src = "images/profile2.png")}
+                        onMouseOut={e => (e.currentTarget.src = "images/profile1.png")} /> */}
                 </div>
             </div>
             {/* <div className="row bottom-row">
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,112 +4,83 @@ function Portfolio() {
     return (
         <div className="container">
             <div className="row main-row">
-                <div className="col-md-12 carousel-div">
-                    <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
-                        <div className="carousel-inner">
-                            <div className="carousel-item active" data-interval="4000">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-9 img-div">
-                                            <img src="images/hangman.png" className="d-block w-100" alt="..." />
-                                        </div>
-                                        <div className="col-md-3 info-div">
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="carousel-item" data-interval="4000">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-9 img-div">
-                                            <img src="images/green-street.png" className="d-block w-100" alt="..." />
-                                        </div>
-                                        <div className="col-md-3 info-div">
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="carousel-item" data-interval="4000">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-9 img-div">
-                                            <img src="images/our_house.png" className="d-block w-100" alt="..." />
-                                        </div>
-                                        <div className="col-md-3 info-div">
-
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                            </div>
-                            <div className="carousel-item" data-interval="4000">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-9 img-div">
-
-                                            <img src="images/doodle_poots.png" className="d-block w-100" alt="..." />
-                                        </div>
-                                        <div className="col-md-3 info-div">
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="carousel-item" data-interval="4000">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-9 img-div">
-
-                                            <img src="images/bamazon.png" className="d-block w-100" alt="..." />
-                                        </div>
-                                        <div className="col-md-3 info-div">
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="carousel-item" data-interval="4000">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-9 img-div">
-
-                                            <img src="images/train_sched.png" className="d-block w-100" alt="..." />
-                                        </div>
-                                        <div className="col-md-3 info-div">
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="carousel-item" data-interval="4000">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-md-9 img-div">
-
-                                            <img src="images/burger.png" className="d-block w-100" alt="..." />
-                                        </div>
-                                        <div className="col-md-3 info-div">
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Next</span>
-                        </a>
+                <div className="col-md-9 img-div">
+                    <img src="images/green-street.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="col-md-3 info-col">
+                    <div className="info-div">
+                        <h3 className="info-name">Green Street</h3>
+                        <p className="info">Role: Original Concept/Front-End</p>
+                        <p className="info">An app that allows gardeners to connect to form tiny little front yard farmer's markets and consumers to find the market's homepage.</p>
+                        <p className="info">Primary Technologies: MERN (MongoDB, Express, React and Node.js</p>
+                        <p className="info">Github Repo</p>
+                        <p className="info">Website</p>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div className="row main-row">
+                <div className="col-md-9 img-div">
+                    <img src="images/hangman.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="col-md-3 info-div">
+
+                </div>
+            </div>
+
+
+            <div className="row main-row">
+                <div className="col-md-9 img-div">
+                    <img src="images/our_house.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="col-md-3 info-div">
+
+                </div>
+            </div>
+
+
+
+
+            <div className="row main-row">
+                <div className="col-md-9 img-div">
+
+                    <img src="images/doodle_poots.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="col-md-3 info-div">
+
+                </div>
+            </div>
+
+
+            <div className="row main-row">
+                <div className="col-md-9 img-div">
+
+                    <img src="images/bamazon.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="col-md-3 info-div">
+
+                </div>
+            </div>
+
+
+            <div className="row main-row">
+                <div className="col-md-9 img-div">
+
+                    <img src="images/train_sched.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="col-md-3 info-div">
+
+                </div>
+            </div>
+            <div className="row main-row">
+                <div className="col-md-9 img-div">
+                    <img src="images/burger.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="col-md-3 info-div">
+
+                </div>
+            </div>
+        </div >
     )
 }
 
