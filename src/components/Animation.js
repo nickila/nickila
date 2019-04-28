468e24a9c06a370f6946af8d9dfdb92a6854d274
Brian Nickila <brian.nickila@gmail.com>
2019-04-28 09:23:07 -0500

    Added alt props on imagemages. Working on responsiveness.

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,16 +1,93 @@
-import React from "react";
+import React, { Component } from "react";
+import puffies from "../animation/puffies.jpg";
+import chimney from "../animation/chimneysquare.jpg";
+import facedoh from "../animation/face-doh.jpg";
+import dog from "../animation/dogSQ.jpg";
+import gnome from "../animation/gnomejump.jpg";
+import bird from "../animation/bird.jpg";
+import name from "../animation/name.jpg";
+import monter from "../animation/monter.jpg";
+import cupcake from "../animation/cupcake.jpg";
 
-function Animation() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-3">
+import puffiesGif from "../animation/newpuff.gif";
+import chimneyGif from "../animation/chimneysquare.gif";
+import facedohGif from "../animation/face-doh.gif";
+import dogGif from "../animation/dogSQ.gif";
+import monterGif from "../animation/monter.gif";
+import birdGif from "../animation/bird.gif";
+import gnomeGif from "../animation/gnome2.gif";
+import nameGif from "../animation/nameline2.gif";
+import cupcakeGif from "../animation/cupcakescream.gif";
 
+class Animation extends Component {
+
+    state = {
+        gif: monterGif
+    }
+
+    getGif = e => {
+        e.preventDefault()
+        let value = e.target.name
+        console.log(value)
+        this.setState({
+            gif: value
+        })
+    }
+    render() {
+        return (
+            <div className="container" >
+                <div className="row">
+                    <div className="col-md-8">
+                        <div className="container">
+                            <div className="row main-row animation-row">
+                                <h1 className="name">Animation</h1>
+                            </div>
+                            <div className='row'>
+                                <div>
+                                    <img className="col-md-6 stage" src={this.state.gif} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="container">
+                            <div className="row deck-row">
+                                <div className="col-md-4 gif-col">
+                                    <img className="deck-img" src={facedoh} name={facedohGif} onClick={this.getGif} alt="face-doh gif" />
+                                </div>
+                                <div className="col-md-4 gif-col">
+                                    <img className="deck-img" src={chimney} name={chimneyGif} onClick={this.getGif} alt="chimney sweater gif" />
+                                </div>
+                                <div className="col-md-4 gif-col">
+                                    <img className="deck-img" src={dog} name={dogGif} onClick={this.getGif} alt="dog cartoon gif" />
+                                </div>
+                                <div className="col-md-4 gif-col">
+                                    <img className="deck-img" src={bird} name={birdGif} onClick={this.getGif} alt="bird flying gif" />
+                                </div>
+                                <div className="col-md-4 gif-col">
+                                    <img className="deck-img" src={puffies} onClick={this.getGif} name={puffiesGif}
+                                        alt="puffy and hat gif" />
+                                </div>
+                                <div className="col-md-4 gif-col">
+                                    <img className="deck-img" src={gnome} name={gnomeGif} onClick={this.getGif} alt="gnome jumping gif" />
+                                </div>
+                                <div className="col-md-4 gif-col">
+                                    <img className="deck-img" src={name} name={nameGif} onClick={this.getGif} alt="brian nickila gif" />
+                                </div>
+                                <div className="col-md-4 gif-col">
+                                    <img className="deck-img" src={monter} name={monterGif} onClick={this.getGif} alt="monter with sword" />
+                                </div>
+                                <div className="col-md-4 gif-col">
+                                    <img className="deck-img" src={cupcake} name={cupcakeGif} onClick={this.getGif} alt="cupcake gif" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
 
-    )
+        )
+    }
 }
 
 export default Animation;
--- src/components/Name.js
+++ src/components/Name.js
@@ -7,26 +7,16 @@ function Name() {
             <div className="row main-row">
                 <div className="col-md-8">
                     <h1 className="name">Brian Nickila</h1>
-                    <p className="about"><span className="about-bold">Front-End Web Developer</span> creating apps and websites in a creative and user-friendly style in St. Paul, Minnesota. I have a bachelor of fine arts degree from the University of Wisconsin-Stevens Point and a certificate in full-stack web development from the University of Minnesota. When I'm not writing code, you might find me working on <Link className="about-bold link" to="/animation">animation</Link>, playing guitar, gardening or camping. Please check out my <Link className="about-bold link" to="/portfolio">portfolio</Link> and feel free to <Link className="about-bold link" to="/contact">contact</Link> me for any reason. I'd love to hear from you.</p>
+                    <p className="about"><span className="about-bold">Front-End Web Developer</span> creating apps and websites in a creative and user-friendly style in St. Paul, Minnesota. I have a bachelor of fine arts degree from the University of Wisconsin-Stevens Point and a certificate in full-stack web development from the University of Minnesota. When I'm not writing code, you might find me working on <Link className="about-bold link" to="/animation">animation</Link>, playing guitar, gardening or camping. Please check out my <Link className="about-bold link" to="/portfolio">portfolio</Link> and feel free to <a href="mailto:brian.nickila@gmail.com" className="about-bold link" to="/contact">contact</a> me for any reason. I'd love to hear from you.</p>
                     <p className="tech"><span className="about-bold">Technologies:</span> React, JavaScript, HTML, CSS, jQuery, Bootstrap, MongoDB, MySQL, Express, Node.js, Sequelize, Mongoose, Photoshop, Illustrator & Flash</p>
                 </div>
                 <div className="col-md-4 img-div">
-                    <img className="profile" src="images/profile1.png" />
+                    <img className="profile" src="images/profile1.png" alt="brian nickila sketch" />
                     <div className="overlay">
-                        <img className="color-profile" src="images/profile2.png" />
+                        <img className="color-profile" src="images/profile2.png" alt="brian nickila colored sketch" />
                     </div>
-                    {/* <img className="profile" src="images/profile1.png"
-                        onMouseOver={e => (e.currentTarget.src = "images/profile2.png")}
-                        onMouseOut={e => (e.currentTarget.src = "images/profile1.png")} /> */}
                 </div>
             </div>
-            {/* <div className="row bottom-row">
-                <div className="col-md-10">
-                    <h3 className="techHeader">Technologies</h3>
-                    <hr />
-                    <p className="tech">React &nbsp; JavaScript &nbsp; HTML &nbsp; CSS &nbsp; jQuery &nbsp; Bootstrap &nbsp; MongoDB &nbsp; MySQL &nbsp; Express &nbsp; Node.js &nbsp; Sequelize &nbsp; Mongoose &nbsp; Photoshop &nbsp; Illustrator &nbsp; Flash</p>
-                </div>
-            </div> */}
         </div >
     )
 }
